d7358bff5602d9a636f821b27eeb8a7899d8aecb
A.J.S.T. Ligthart <aaron.ligthart@hva.nl>
2019-01-26 13:36:39 +0100

    Added hobo selection screen, added scene switching

--- GGJ2019/Assets/HoboSelector.cs
+++ GGJ2019/Assets/HoboSelector.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HoboSelector : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class HoboSelector : MonoBehaviour
     public void SelectCharacter(int sellectedNum)
     {
         playerNum = sellectedNum;
+        SceneManager.LoadScene(1);
     }
 
 }
