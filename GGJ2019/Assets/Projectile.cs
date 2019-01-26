d2f7ace9a6e3a375f9936357024a019fd8f23e10
A.J.S.T. Ligthart <aaron.ligthart@hva.nl>
2019-01-26 14:39:05 +0100

    Added projectile script, added Bottle projectile

---
+++ GGJ2019/Assets/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private float distanceToPlayer;
+    private GameObject player;
+    public GameObject breakEffect;
+    [SerializeField]
+    private float hitBoxDistance;
+    private Health playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+         playerHealth = player.GetComponent<Health>();
+        distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        print("Distance to other: " + distanceToPlayer);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        print("Distance to other: " + distanceToPlayer);
+
+        if(distanceToPlayer <= hitBoxDistance)
+        {
+            Instantiate(breakEffect, transform.position,transform.rotation);
+            playerHealth.TakeDamage(1);
+
+            Destroy(gameObject);
+        }
+
+    }
+}
