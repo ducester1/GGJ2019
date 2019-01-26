04f82f623a0d3ec07a75adec163342ab298881cb
A.J.S.T. Ligthart <aaron.ligthart@hva.nl>
2019-01-26 10:47:44 +0100

    Added Hobo Selection and spawning

---
+++ GGJ2019/Assets/HoboSelector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoboSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int playerNum;
+
+    public void SelectCharacter(int sellectedNum)
+    {
+        playerNum = sellectedNum;
+    }
+
+}
