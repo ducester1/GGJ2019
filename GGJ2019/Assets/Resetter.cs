add0c144563c376792f3b15d009e7d0e5202940a
Scott <schepers.scott@hotmail.com>
2019-01-27 08:50:40 +0100

    Added: guitar to city, resetter class Changed city position

---
+++ GGJ2019/Assets/Resetter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resetter : MonoBehaviour
+{
+    private bool once;
+    private float minHeight;
+    private GameObject player;
+
+    public Vector3 initPos;
+    public Quaternion initRot;
+    public GameObject asset;
+
+    void Start()
+    {
+        once = false;
+        player = GameObject.Find("Player");
+        minHeight = 0.3f;
+        initPos = this.transform.position;
+        initRot = this.transform.rotation;
+    }
+
+    void Update()
+    {
+        Debug.Log(Vector3.Distance(player.transform.position, transform.position));
+        if (this.transform.position.y < minHeight && Vector3.Distance(player.transform.position, transform.position) > 1.5 && !once)
+        {
+            once = true;
+            Instantiate(asset, initPos, initRot);
+            Destroy(gameObject, 3);
+        }
+    }
+}
