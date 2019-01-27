139928bd2cb3e9b6abe95a2a75c7a1d13cec5cb9
A.J.S.T. Ligthart <aaron.ligthart@hva.nl>
2019-01-27 04:33:51 +0100

    Added Sounds to Aaron

---
+++ GGJ2019/Assets/GiberishSpeaker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class GiberishSpeaker : MonoBehaviour
+{
+    public AudioClip[] mouthSounds;
+    public AudioSource audioSource;
+
+    private int tempSound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        PlaySound(Random.Range(0, mouthSounds.Length));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            print("soundPlaying");
+            PlaySound(Random.Range(0, mouthSounds.Length));
+        }
+
+    }
+
+    public void PlaySound(int index)
+    {
+        audioSource.clip = mouthSounds[index];
+        audioSource.PlayOneShot(audioSource.clip);
+
+    }
+}
